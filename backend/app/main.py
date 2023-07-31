7c43bd553cabb873d134596781d0e26ac3bfe15f
InchaeNa <dlsco1230@gmail.com>
2023-07-31 12:53:21 +0900

    "react-fastapi"

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 from .database import SessionLocal, engine, Base
-from .models.member import User
+from . import models, schemas
 
-# SQLAlchemy 모델의 Base 클래스를 사용해 테이블을 생성
 Base.metadata.create_all(bind=engine)
 
-# 요청마다 사용할 새로운 SQLAlchemy SessionLocal 인스턴스를 생성
 def get_db():
     try:
         db = SessionLocal()
@@ -13,6 +12,15 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-def read_root(db: Session = Depends(get_db)):
-    return {"Hello": "World"}
+app = FastAPI()
+
+@app.post("/signup", response_model=schemas.UserCreate)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
